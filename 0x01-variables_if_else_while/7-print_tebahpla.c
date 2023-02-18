18300f539a3b0a9e0f566b680c99c93906864bc4
AyaaZa <ayazakareya.h@gmail.com>
2023-02-18 09:23:30 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - task 7
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+putchar('\n');
+return (0);
+}
