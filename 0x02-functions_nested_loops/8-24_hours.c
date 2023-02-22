4296f0398b33ffe045733aaec9e2883073296d3a
AyaaZa <ayazakareya.h@gmail.com>
2023-02-22 10:05:52 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - time
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+int m1, m2, h1, h2;
+
+for (h2 = 0; h2 < 3; h2++)
+{
+for (h1 = 0; h1 < 4; h1++)
+{
+for (m2 = 0; m2 < 6; m2++)
+{
+for (m1 = 0; m1 < 10; m1++)
+{
+	_putchar(h2 + '0');
+	_putchar(h1 + '0');
+	_putchar(':');
+	_putchar(m2 + '0');
+	_putchar(m1 + '0');
+	_putchar('\n');
+}
+}
+}
+}
+}
---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * times_table - times
+ * Ruturn: void
+ */
+
+void times_table(void)
+{
+int n;
+int num;
+
+
+}
