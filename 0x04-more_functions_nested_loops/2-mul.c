2867367da25e0b30a6fabb8e58a60bc92b369db1
AyaaZa <ayazakareya.h@gmail.com>
2023-02-23 12:02:28 -0800

    error

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b:second int
+ * Return: as shown
+ */
+
+
+
+int mul(int a, int b)
+{
+int c;
+
+c = a * b;
+	_putchar(c);
+	return (c);
+}
